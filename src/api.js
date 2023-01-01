60698cf9503386e2cf8c07f4cc1e6b7ef8c31847
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2023-01-01 11:30:30 -0300

    header refactor

--- src/api.js
+++ src/api.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const searchImages = async () => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
-        headers:{
+        headers: {
             Authorization: 'Client-ID WFlFPOAVreB6WroKbaErZhFMyid1oJhlocHb41gubFQ'
-        }, 
+        },
         params: {
             query: 'cars'
         }
